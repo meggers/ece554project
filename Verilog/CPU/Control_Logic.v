c8d10d1f8a9088d983df661ef30a3f2613e262f2
gnygard <gnygard@wisc.edu>
2015-10-18 15:12:07 -0500

    updated control logic

--- Verilog/CPU/Control_Logic.v
+++ Verilog/CPU/Control_Logic.v
@@ -4,23 +4,15 @@ module Control_Logic(instruction, /*opcode,*/
 
 	);
 
-//INPUTS
+//INPUTS////////////////////////////////////////////////////
+
 input  [5:0]  opcode; // Used for differentiating between NO_OP and HALT
 
+//OUTPUTS///////////////////////////////////////////////////
 
-//OUTPUTS
-output 	reg    		data_reg;	/* Control signal to Regfile to
-                                    	specifiy the contents of the 
-                                    	Data Segment Register for
-                                    	supplying read_data_1 */
-output 	reg		    stack_reg;		/* Control signal to RegFile to
-                                    	specify the contents of the
-                                    	Stack_Pointer Register for
-                                    	supplying read_data_1 */
 output reg	jump;
 output reg	branch;		// branching control; 0-2 sensitive, 3 pick 
-//output reg	mem_to_reg;	// LW signal to Memory unit 
-//output reg	[2:0] alu_op;	// ALU control unit input
+output reg	mem_to_reg;	// LW signal to Memory unit 
 output reg	[1:0] alu_src;	// ALU operand seleciton
 output reg	sign_ext_sel;	// sign extend select bit
 output reg	reg_rt_src;	// Read_reg_2 proper SW select
@@ -28,21 +20,25 @@ output reg	RegWrite;	// Signal for writing back to register
 output reg	MemWrite;	// Signal for writing to memory
 output reg	OAMWrite;	// Signal for writing to the OAM
 output reg	MemRead;	// Signal for reading from memory
-output reg	load_half;	// Specifies the ALU result
-output reg	half_spec;	// (0 -> LHB, 1 -> LLB)
 
 output reg	HALT;		// STOP THE CPU
               
 /* LOCAL PARAMS */      
 //ALU OPERATIONS 
-localparam   ADD   =   4'b0000;
-localparam   SUB   =   4'b0001;
-localparam   NAND  =   4'b0010;
-localparam   XOR   =   4'b0011;
-localparam   INC   =   4'b0100;
-localparam   SRA   =   4'b0101;
-localparam   SRL   =   4'b0110;
-localparam   SLL   =   4'b0111;
+localparam	ADD   = 6'b000000;
+localparam	ADDI  = 6'b000001;
+localparam	SUB   = 6'b000010;
+localparam	NAND  = 6'b000011;
+localparam	XOR   = 6'b001000;
+
+
+localparam   NAND  =   6'b0010;
+localparam   XOR   =   6'b0011;
+localparam   INC   =   6'b0100;
+localparam   SRA   =   6'b0101;
+localparam   SRL   =   6'b0110;
+localparam   SLL   =   6'b0111;
+
 //SPECIAL
 localparam   LW    =   4'b1000;
 localparam   SW    =   4'b1001;
@@ -52,137 +48,147 @@ localparam   B     =   4'b1100;
 localparam   CALL  =   4'b1101;
 localparam   RET   =   4'b1110;
 localparam   ERR   =   4'b1111;
-//BRANCH CONTROL
-localparam   EQ    =   3'b000;
-localparam   LT    =   3'b001;
-localparam   GT    =   3'b010;
-localparam   OV    =   3'b011;
-localparam   NE    =   3'b100;
-localparam   GE    =   3'b101;
-localparam   LE    =   3'b110;
-localparam   TR    =   3'b111;
 
   always_comb begin
       
-   opcode = instruction[15:12];
-       
 	case(opcode)
-              
-   	   ADD : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
            
-           SUB : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
+	// ADD and ADDI   
+	(~&opcode[5:1]) : begin	
 
-           NAND : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
+		jump		= 1'b0;
+		branch		= 1'b0;
+		mem_to_reg	= 1'b0;
+		sign_ext_sel	= 1'b1;
+		reg_rt_src	= 1'b0;
+		RegWrite	= 1'b1;
+		MemWrite	= 1'b0;
+		OAMWrite	= 1'b0;
+		MemRead		= 1'b0;
+
+		if (opcode[0]) begin
+			alu_src	= 2'b01;
+		end
+		else begin
+			alu_src = 2'b00;
+		end	
+
+	end
            
-           XOR : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
+	SUB : begin
+		
+		jump		= 1'b0;
+		branch		= 1'b0;
+		mem_to_reg	= 1'b0;
+		sign_ext_sel	= 1'b0;
+		reg_rt_src	= 1'b0;
+		RegWrite	= 1'b1;
+		MemWrite	= 1'b0;
+		OAMWrite	= 1'b0;
+		MemRead		= 1'b0;
+		alu_src 	= 2'b00;
+
+	end
+
+	NAND : begin
+		
+		jump		= 1'b0;
+		branch		= 1'b0;
+		mem_to_reg	= 1'b0;
+		sign_ext_sel	= 1'b0;
+		reg_rt_src	= 1'b0;
+		RegWrite	= 1'b1;
+		MemWrite	= 1'b0;
+		OAMWrite	= 1'b0;
+		MemRead		= 1'b0;
+		alu_src 	= 2'b00;
+
+	end
+
+	// AND and ANDI
+	(~&opcode[5:3] & opcode[2] & ~opcode[1]) : begin	
+
+		jump		= 1'b0;
+		branch		= 1'b0;
+		mem_to_reg	= 1'b0;
+		sign_ext_sel	= opcode[0];
+		reg_rt_src	= 1'b0;
+		RegWrite	= 1'b1;
+		MemWrite	= 1'b0;
+		OAMWrite	= 1'b0;
+		MemRead		= 1'b0;
+
+		if (opcode[0]) begin
+			alu_src	= 2'b01;
+		end
+		else begin
+			alu_src = 2'b00;
+		end	
+
+	end
+  
+	XOR : begin
+
+		jump		= 1'b0;
+		branch		= 1'b0;
+		mem_to_reg	= 1'b0;
+		sign_ext_sel	= 1'b0;
+		reg_rt_src	= 1'b0;
+		RegWrite	= 1'b1;
+		MemWrite	= 1'b0;
+		OAMWrite	= 1'b0;
+		MemRead		= 1'b0;
+		alu_src 	= 2'b00;
+		
+	end
                              
-           INC : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b1;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
+	BEQ : begin
+
+		jump		= 1'b0;
+		branch		= 1'b1;
+		mem_to_reg	= 1'b0;
+		sign_ext_sel	= 1'b1;
+		reg_rt_src	= 1'b0;
+		RegWrite	= 1'b0;
+		MemWrite	= 1'b0;
+		OAMWrite	= 1'b0;
+		MemRead		= 1'b0;
+		alu_src 	= 2'b01;
+
+	end
            
-          SRA : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
+	BNE : begin
+
+		jump		= 1'b0;
+		branch		= 1'b1;
+		mem_to_reg	= 1'b0;
+		sign_ext_sel	= 1'b1;
+		reg_rt_src	= 1'b0;
+		RegWrite	= 1'b0;
+		MemWrite	= 1'b0;
+		OAMWrite	= 1'b0;
+		MemRead		= 1'b0;
+		alu_src 	= 2'b01;
+	end
            
-           SRL : begin
+	BLT : begin
+
+		jump		= 1'b0;
+		branch		= 1'b1;
+		mem_to_reg	= 1'b0;
+		sign_ext_sel	= 1'b1;
+		reg_rt_src	= 1'b0;
+		RegWrite	= 1'b0;
+		MemWrite	= 1'b0;
+		OAMWrite	= 1'b0;
+		MemRead		= 1'b0;
+		alu_src 	= 2'b01;
+
+	end
+        
+	// All forms of JUMP
+	(~&opcode[5:4] & &opcode[3:2]) : begin
+
 		 data_reg = 1'b0;
 		 stack_reg = 1'b0;
 		 call = 1'b0;
@@ -199,159 +205,10 @@ localparam   TR    =   3'b111;
 		 load_half = 1'b0;
 		 half_spec = 1'b0;
 		 HALT = 1'b0;
-		 end
-           
-           SLL : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
 
-	   LW :  begin
-		 data_reg = 1'b1;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b1;
-		 alu_op = ADD[2:0];
-		 alu_src = 1'b1;
-		 sign_ext_sel = 1'b1;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b1;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
+	end
 
-	   SW :  begin
-		 data_reg = 1'b1;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = ADD[2:0];
-		 alu_src = 1'b1;
-		 sign_ext_sel = 1'b1;
-		 reg_rt_src = 1'b1;
-		 RegWrite = 1'b0;
-		 MemWrite = 1'b1;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
-
-	   LHB : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b1;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b1;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
-
-	   LLB : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = opcode[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b1;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b1;
-		 half_spec = 1'b1;
-		 HALT = 1'b0;
-		 end
-
-	   B :   begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b0;
-		 call = 1'b0;
-		 rtrn = 1'b0;
-		 branch = 1'b1;
-		 mem_to_reg = 1'b0;
-		 alu_op = 3'bzzz;
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b1;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b0;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
-
-	   CALL : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b1;
-		 call = 1'b1;
-		 rtrn = 1'b0;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = ADD[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b1;
-		 MemRead  = 1'b0;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
-
-	   RET : begin
-		 data_reg = 1'b0;
-		 stack_reg = 1'b1;
-		 call = 1'b0;
-		 rtrn = 1'b1;
-		 branch = 1'b0;
-		 mem_to_reg = 1'b0;
-		 alu_op = ADD[2:0];
-		 alu_src = 1'b0;
-		 sign_ext_sel = 1'b0;
-		 reg_rt_src = 1'b0;
-		 RegWrite = 1'b1;
-		 MemWrite = 1'b0;
-		 MemRead  = 1'b1;
-		 load_half = 1'b0;
-		 half_spec = 1'b0;
-		 HALT = 1'b0;
-		 end
+	
 
 	   default : begin    //ERR is now defaulted
 	    data_reg = 1'b0;
