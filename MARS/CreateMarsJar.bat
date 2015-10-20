6610ce04cae4f21e2a2b0cfd53452bfea3cff37f
jrroy11 <jrroy11@gmail.com>
2015-10-20 15:57:53 -0500

    Updated MARS MAKE file

--- MARS/CreateMarsJar.bat
+++ MARS/CreateMarsJar.bat
@@ -1,1 +1,3 @@
+javac mars/mips/instructions/*.java -Xlint
+
 jar cmf mainclass.txt Mars.jar PseudoOps.txt Config.properties Syscall.properties Settings.properties MARSlicense.txt mainclass.txt MipsXRayOpcode.xml registerDatapath.xml controlDatapath.xml ALUcontrolDatapath.xml CreateMarsJar.bat Mars.java Mars.class docs help images mars 
