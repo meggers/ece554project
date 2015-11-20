0d8e4d3638feaba8a0e425592540ffda3653dd9c
jrroy11 <jrroy11@gmail.com>
2015-11-19 18:55:08 -0600

    Started work on tron in our ISA

--- MARS/asm_test.asm
+++ MARS/asm_test.asm
@@ -1,26 +1,28 @@
+.word 0x00000000
+
 .text
-	beq $t0, $t0, quit
-	bne $t0, $t0, quit
-	blt $t0, $t0, quit
+	beq $t0,$t0,quit
+	bne $t0,$t0,quit
+	blt $t0,$t0,quit
 	#b quit
 	call quit
 	Return
 	
-	lw $t0, 0x40
-	li $t0, 6
+	lw $t0,0x40
+	li $t0,6
 	pop $t5
-	sw $t0, 0x40
+	sw $t0,0x40
 	push $t0
 	
-	add $t0, $t0, $t0
-	addi $t0, $t0, 1
-	sub $t0, $t0, $t0
-	nand $t0, $t0, $t0
-	and $t0, $t0, $t0
-	andi $t0, $t0, 1
-	srl $t0, $t0, 2
-	sll $t0, $t0, 2
-	xor $t0, $t0, $t0
+	add $t0,$t0,$t0
+	addi $t0,$t0,1
+	sub $t0,$t0,$t0
+	nand $t0,$t0,$t0
+	and $t0,$t0,$t0
+	andi $t0,$t0,1
+	srl $t0,$t0,2
+	sll $t0,$t0,2
+	xor $t0,$t0,$t0
 
 
 
