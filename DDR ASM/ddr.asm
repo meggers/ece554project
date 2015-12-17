52b1dbbd9216d454491c7c2a2480afba87293d7f
Kai Zhao <kaizhao@dyn-72-33-146-123.uwnet.wisc.edu>
2015-12-16 23:20:02 -0600

    just added a bunch of nops to ddr

--- DDR ASM/ddr.asm
+++ DDR ASM/ddr.asm
@@ -10,19 +10,23 @@
 	li $s3, 0
 	li $t7, 16
 	call displayScore
+	nop
 	b main
 	
 	displayScore:
+		nop
 		li $t4, 0
 		addi $s3, $s3, 0
 		blt resetAndReturnFromDisplayScore
 		add $t3, $0, $s3
 		startDivide:
+			nop
 			addi $t3, $t3, -10
 			blt doneDivide
 			addi $t4, $t4, 1
 			b startDivide
 		doneDivide:
+			nop
 			addi $t3, $t3, 10
 			addi $0, $t4, -9
 			beq display90
@@ -44,6 +48,7 @@
 			beq display10
 			li $t4, char_0_index
 		endDisplay10s:
+			nop
 			li $t5, 30
 			sbt $t5, $t4
 			li $t4, 2
@@ -68,71 +73,88 @@
 			beq display1
 			li $t4, char_0_index
 		endDisplay1s:
+			nop
 			li $t5, 31
 			sbt $t5, $t4
 			li $t4, 2
 			sba $t5, $t4
 			ret
 	display90:
+		nop
 		li $t4, char_9_index
 		b endDisplay10s
 	display80:
+		nop
 		li $t4, char_8_index
 		b endDisplay10s
 	display70:
+		nop
 		li $t4, char_7_index
 		b endDisplay10s
 	display60:
+		nop
 		li $t4, char_6_index
 		b endDisplay10s
 	display50:
+		nop
 		li $t4, char_5_index
 		b endDisplay10s
 	display40:
+		nop
 		li $t4, char_4_index
 		b endDisplay10s
 	display30:
+		nop
 		li $t4, char_3_index
 		b endDisplay10s
 	display20:
+		nop
 		li $t4, char_2_index
 		b endDisplay10s
 	display10:
+		nop
 		li $t4, char_1_index
 		b endDisplay10s
 		
 	display9:
+		nop
 		li $t4, char_9_index
 		b endDisplay1s
 	display8:
+		nop
 		li $t4, char_8_index
 		b endDisplay1s
 	display7:
+		nop
 		li $t4, char_7_index
 		b endDisplay1s
 	display6:
+		nop
 		li $t4, char_6_index
 		b endDisplay1s
 	display5:
+		nop
 		li $t4, char_5_index
 		b endDisplay1s
 	display4:
+		nop
 		li $t4, char_4_index
 		b endDisplay1s
 	display3:
+		nop
 		li $t4, char_3_index
 		b endDisplay1s
-	display2:
+	display2: nop
 		li $t4, char_2_index
 		b endDisplay1s
-	display1:
+	display1: nop
 		li $t4, char_1_index
 		b endDisplay1s
 		
-	resetAndReturnFromDisplayScore:
+	resetAndReturnFromDisplayScore: nop
 		li $s3, 0
 		ret
-	main:
+	main: nop
 		li $t2, 2
 		li $t0, 24
 		li $t1, char_S_index
@@ -179,7 +201,7 @@
 		li $t0, 160
 		li $t1, background1100_index
 		li $t2, 3
-		addLine:
+		addLine: nop
 		sbt $t0, $t1
 		sba $t0, $t2
 		subi $t0, $t0, -1
@@ -194,14 +216,15 @@
 		sfa $s1, $s0
 		li $s0, 12524 # 256*8*6+254
 
-waitForInterrupt:
+waitForInterrupt: nop
 	nop
 	b 0
+	nop
 	#add $t7, $t7, 1
 	#nop
 	b waitForInterrupt
 	
-game_loop:
+game_loop: nop
 	nop
 	add $t0, $0, $s0
 	addi $t0, $t0, -1
@@ -214,15 +237,15 @@ game_loop:
 	ssl $s1, $s0
 	jr $epc
 
-handle_key_press:
+handle_key_press: nop
 	nop
 	sub $0, $t1, $idr
 	beq mayBeAddPoint
-	subtractPoint:
+	subtractPoint: nop
 		addi $s3, $s3, -1
 		b doNextLetter
 
-	mayBeAddPoint:
+	mayBeAddPoint: nop
 		add $s2, $0, $s0
 		andi $s2, 0xFF
 		li $t6, 44
@@ -231,14 +254,14 @@ handle_key_press:
 		addi $s3, $s3, -1
 		b doNextLetter
 	
-	addPoint:
+	addPoint: nop
 		nop
 		addi $s3, $s3, 1
 		
-	doNextLetter:
+	doNextLetter: nop
 		li $s2, -1
 		call displayScore
-	doNextLetterFromGameLoop:
+	doNextLetterFromGameLoop: nop
 		xor $s2, $s2, $s3
 		andi $s2, 0x03
 		li $s1, 0
@@ -251,7 +274,7 @@ handle_key_press:
 		sub $0, $s2, $s1
 		beq loadS
 	
-	loadD:
+	loadD: nop
 		li $t1, ddr_D_index
 		li $s1, 0
 		sft $s1, $t1
@@ -259,7 +282,7 @@ handle_key_press:
 		li $s0, 49406 # 256*8*6*3+254
 		jr $epc
 
-	loadA:
+	loadA: nop
 		li $t1, ddr_A_index
 		li $s1, 0
 		sft $s1, $t1
@@ -267,7 +290,7 @@ handle_key_press:
 		li $s0, 12524 # 256*8*6+254
 		jr $epc
 		
-	loadW:
+	loadW: nop
 		li $t1, ddr_W_index
 		li $s1, 0
 		sft $s1, $t1
@@ -275,7 +298,7 @@ handle_key_press:
 		li $s0, 24830 # 256*8*6*2+254
 		jr $epc
 		
-	loadS:
+	loadS: nop
 		li $t1, ddr_S_index
 		li $s1, 0
 		sft $s1, $t1
@@ -283,23 +306,24 @@ handle_key_press:
 		li $s0, 37118 # 256*8*6*3+254
 		jr $epc
 
-before_game_loop:
+before_game_loop: nop
 	li $t7, 2
 	b game_loop
 	
 	nop
-game_tick_interrupt:
+game_tick_interrupt: nop
 	nop
 	addi $t7, $t7, -1
 	blt before_game_loop
+	nop
 	jr $epc
 	
 	nop
-keyboard_interrupt:
+keyboard_interrupt: nop
 	nop
 	b handle_key_press
 	
 	nop
-stack_ov_interrupt:
+stack_ov_interrupt: nop
 	nop
 	jr $epc
